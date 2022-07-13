f2109bf5c21a6045708c5f58630069d4697e8251
cesar-lima <cesarr340@gmail.com>
2022-07-12 22:26:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Links from './components/Links'
+import './styles/global.css'
+
+function App() {
+  return (
+    <div>
+      <img src="https://github.com/cesar-lima.png" alt="foto de perfil" />
+      <h1>César Lima</h1>
+      <p>@cesinha.lc</p>
+
+      <Links />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Links.tsx
@@ -0,0 +1,21 @@
+import '../styles/links.css'
+
+function Links() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="https://github.com/cesar-lima" target="_blank">🐱 Github</a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/cesar-lima-correia/" target="_blank">🖱️ Linkedin</a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/cesinha.lc/" target="_blank">📷 Instagram</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Links
