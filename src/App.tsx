238371a0104f659e56632fc8a05681d5a18f35ff
cesarr340 <cesarr340@gmail.com>
2024-11-26 15:08:58 +0000

    trocar favicon da página e cor dos links

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div>
       <img src="https://github.com/cesar-lima.png" alt="foto de perfil" />
       <h1>César Lima</h1>
-      <p>@cesinha.lc</p>
+      <p>Front-End Developer</p>
 
       <Links />
     </div>
