cd9cfc11f4299a6532908899fccee3a0967f780a
cesarr340 <cesarr340@gmail.com>
2024-11-27 12:54:40 +0000

    adicionar mais um link na página e mudar estilo dos botões

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,6 +2,7 @@ import '../styles/links.css'
 import github from '../assets/github.svg'
 import linkedin from '../assets/linkedin.svg'
 import instagram from '../assets/instagram.svg'
+import portfolio from '../assets/portfolio.svg'
 
 function Links() {
     return (
@@ -9,19 +10,25 @@ function Links() {
             <ul>
                 <li>
                     <a href="https://github.com/cesar-lima" target="_blank">
-                        <img src={github} alt="logo github" />
+                        <img src={github} alt="github icon" />
                         Github
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/cesar-lima-correia/" target="_blank">
-                        <img src={linkedin} alt="logo linkedin" />
+                        <img src={linkedin} alt="linkedin icon" />
                         Linkedin
                     </a>
                 </li>
                 <li>
+                    <a href="https://cesar-dev.netlify.app" target="_blank">
+                        <img src={portfolio} alt="portfolio icon" />
+                        Portfolio
+                    </a>
+                </li>
+                <li>
                     <a href="https://www.instagram.com/cesinha.lc/" target="_blank">
-                        <img src={instagram} alt="logo instagram" />
+                        <img src={instagram} alt="instagram icon" />
                         Instagram
                     </a>
                 </li>
